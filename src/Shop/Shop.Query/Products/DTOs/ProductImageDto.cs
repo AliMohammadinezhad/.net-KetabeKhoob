2b23e932239f8c99f2aa52ff4603b91968d7afed
Ali Mohammadinezhad <alimn1382@hotmail.com>
2025-01-29 14:25:12 +0330

    Implement Products Query With CQRS

---
+++ src/Shop/Shop.Query/Products/DTOs/ProductImageDto.cs
@@ -0,0 +1,10 @@
+﻿using Common.Query;
+
+namespace Shop.Query.Products.DTOs;
+
+public class ProductImageDto : BaseDto
+{
+    public long ProductId { get; set; }
+    public string ImageName { get; set; }
+    public int Order { get; set; }
+}
---
+++ src/Shop/Shop.Query/Products/GetByFilter/GetProductByFilterQuery.cs
@@ -0,0 +1,56 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Products.DTOs;
+
+namespace Shop.Query.Products.GetByFilter;
+
+public class GetProductByFilterQuery : QueryFilter<ProductFilterResult, ProductFilterParams>
+{
+    public GetProductByFilterQuery(ProductFilterParams filterParams) : base(filterParams)
+    {
+    }
+}
+
+public class GetProductByFilterQueryHandler : IQueryHandler<GetProductByFilterQuery, ProductFilterResult>
+{
+    private readonly ShopContext _context;
+
+    public GetProductByFilterQueryHandler(ShopContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ProductFilterResult> Handle(GetProductByFilterQuery request, CancellationToken cancellationToken)
+    {
+        var @params = request.FilterParams;
+        var result = _context.Products.OrderByDescending(x => x.Id).AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(@params.Slug))
+            result = result.Where(x => x.Slug.Equals(@params.Slug));
+
+        if (!string.IsNullOrWhiteSpace(@params.Title))
+            result = result.Where(x => x.Title.Contains(@params.Title));
+
+        if (@params.Id is not null or 0)
+            result = result.Where(x => x.Id.Equals(@params.Id));
+
+        var skip = (@params.PageId - 1) * @params.Take;
+        var model = new ProductFilterResult
+        {
+            Data = result
+                .Skip(skip)
+                .Take(@params.Take)
+                .Select(x => x.MapListData())
+                .ToList(),
+            FilterParams = @params
+
+        };
+
+        model.GeneratePaging(result, @params.Take, @params.PageId);
+        return model;
+
+
+    }
+}
+

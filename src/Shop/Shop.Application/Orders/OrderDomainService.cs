9808a831d5872d0c403292cc47a60cb686678499
Ali Mohammadinezhad <alimn1382@hotmail.com>
2025-01-29 10:30:14 +0330

    add DomainService Implementation Files

---
+++ src/Shop/Shop.Application/Orders/OrderDomainService.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.OrderAgg.Services;
+
+namespace Shop.Application.Orders;
+
+public class OrderDomainService : IOrderDomainService
+{
+    public bool IsWantedOrderItemCountExistInInventory(long inventoryId, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsOrderItemExistInInventory(long inventoryId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Shop/Shop.Application/Sellers/SellerDomainService.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Domain.SellerAgg;
+using Shop.Domain.SellerAgg.Services;
+
+namespace Shop.Application.Sellers;
+
+public class SellerDomainService : ISellerDomainService
+{
+    public bool CheckSellerInformation(Seller seller)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool NationalCodeExistInDataBase(string nationalCode)
+    {
+        throw new NotImplementedException();
+    }
+}

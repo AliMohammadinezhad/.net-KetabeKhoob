0ec0fc6d433fbe0864fa591767eae2860a5a21e4
Ali Mohammadinezhad <alimn1382@hotmail.com>
2025-07-21 13:27:42 +0330

    bugfix: fix Remove Category bug in remove category handler

--- src/Shop/Shop.Application/Categories/Remove/RemoveCategoryCommand.cs
+++ src/Shop/Shop.Application/Categories/Remove/RemoveCategoryCommand.cs
@@ -17,8 +17,10 @@ public class RemoveCategoryCommandHandler : IBaseCommandHandler<RemoveCategoryCo
     public async Task<OperationResult> Handle(RemoveCategoryCommand request, CancellationToken cancellationToken)
     {
         var result = await _categoryRepository.DeleteCategory(request.CategoryId);
-        if (result)
-            return OperationResult.Success();
-        return OperationResult.Error("امکان حذف این دسته بندی وجود ندارد.");
+        if (!result)
+            return OperationResult.Error("امکان حذف این دسته بندی وجود ندارد.");
+
+        await _categoryRepository.Save();
+        return OperationResult.Success();
     }
 }
